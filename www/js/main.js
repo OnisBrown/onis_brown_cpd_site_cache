adc4f2ba550fd3323aee530a82a227c8c2936655
OnisBrown <Onis.Brown@hotmail.co.uk>
2018-01-25 15:04:41 +0000

    setup

---
+++ www/js/main.js
@@ -0,0 +1,10 @@
+// use when testing phone gap as will not get fired in browser
+document.addEventListener("deviceready", setup, false);
+function setup() {
+ navigator.notification.alert(
+ 'You are the winner!', // message
+ alertDismissed, // callback
+ 'Game Over', // title
+ 'Done' // buttonName
+ );
+};
